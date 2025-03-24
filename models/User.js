f49dc99246b91d7580f8bc2fb496247e8f9fccf6
Phonlakrit Chaengtamtham <phonlakritc@gmail.com>
2025-03-24 13:13:57 +0700

    fix show name, email, tel

--- models/User.js
+++ models/User.js
@@ -52,9 +52,16 @@ UserSchema.pre('save',async function(next) {
 
 //Sign JWT and return
 UserSchema.methods.getSignedJwtToken=function() {
-    return jwt.sign({id:this._id},process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRE
-    });
+    return jwt.sign(
+        {
+            id: this._id,
+            name: this.name,
+            email: this.email,
+            tel: this.tel
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRE }
+    );
 }
 
 //Match user entered password to hashed password in database
